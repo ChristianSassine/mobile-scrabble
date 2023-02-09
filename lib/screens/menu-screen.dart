812a68c323e53f04011ea1c17b7003da4f9d08fb
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2023-02-09 00:02:56 -0500

    Added input + buttons for auth on homescreen

--- lib/screens/menu-screen.dart
+++ lib/screens/menu-screen.dart
@@ -3,16 +3,21 @@ import 'package:get_it/get_it.dart';
 import 'package:mobile/screens/chat-screen.dart';
 import 'package:mobile/domain/services/chat-service.dart';
 
-class MenuScreen extends StatelessWidget {
+class MenuScreen extends StatefulWidget {
   const MenuScreen({super.key, required this.title});
 
   final String title;
 
   @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
@@ -20,22 +25,47 @@ class MenuScreen extends StatelessWidget {
           children: [
             Image.asset("assets/images/scrabble_logo.png", width: 500),
             const SizedBox(height: 100),
-            // SizedBox(
-            //   width: 300,
-            //   child: TextFormField(
-            //
-            //   ),
-            // ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 300,
+                      child: Form(
+                        child: TextFormField(
+                          onFieldSubmitted: (String msg) {
+                            // submitMessage(msgController.text);
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Ecrivez votre nom d'utilisateur ici",
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Colors.green), onPressed: () {}, child: Text("Sign in")),
+                    const SizedBox(height: 5),
+                    ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Colors.red), onPressed: () {}, child: Text("Log out")),
+                  ],
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 30),
             ElevatedButton(
-                onPressed: false ? null : () {
-                  GetIt.I<ChatService>().joinRoom("Gary Anderson");
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            const ChatScreen(title: "Prototype: Salle de clavarage")),
-                  );
-                },
+                onPressed: false
+                    ? null
+                    : () {
+                        GetIt.I<ChatService>().joinRoom("Gary Anderson");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ChatScreen(
+                                  title: "Prototype: Salle de clavarage")),
+                        );
+                      },
                 child: Text("Rejoindre une salle de clavardage")),
           ],
         ),
