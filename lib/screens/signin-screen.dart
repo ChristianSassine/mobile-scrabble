36ba2246b6fd7a6ff84e007b1df15c3007f770bc
yasskadd <yasser.kadimi@gmail.com>
2023-03-11 15:32:46 -0500

    UI Done

--- lib/screens/signin-screen.dart
+++ lib/screens/signin-screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key, required this.title});
-
   final String title;
 
   @override
@@ -10,68 +9,115 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
-
   // Form objects
-  final _usernameFieldController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-
-  void _validateAndCreateAccount() {
-    //TODO: Validate that the username wasn't already taken
-    if (_formKey.currentState!.validate()) {
-      //TODO: Call the account creation service
-      Navigator.pop(context);
-    }
-  }
-
-  String? _validateUsername(username) {
-    if (username == null || username.isEmpty || username.trim() == '') {
-      return "Entrez un nom d'utilisateur avant de soumettre";
-    }
-    return null;
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: 300,
-                      child: Form(
-                        key: _formKey,
-                        child: TextFormField(
-                          validator: _validateUsername,
-                          controller: _usernameFieldController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: "Ecrivez votre nom d'utilisateur ici",
-                          ),
-                          onFieldSubmitted: (_) => _validateAndCreateAccount(),
-                        ),
-                        //TODO: Add text field for password, email and later avatar selection
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: _validateAndCreateAccount,
-                      child: const Text("Créer un compte"),
-                    )
-                  ],
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: const Scaffold(
+          body: Center(
+            child: SizedBox(
+              width: 500,
+              height: 600,
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: SignUpForm(),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ));
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final _emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(height: 16),
+          const Text('Email'),
+          TextFormField(
+            controller: _emailController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your email';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          const Text('Username'),
+          TextFormField(
+            controller: _usernameController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your username';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          const Text('Password'),
+          TextFormField(
+            controller: _passwordController,
+            obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 16),
+          const Text('Confirm Password'),
+          TextFormField(
+            controller: _confirmPasswordController,
+            obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please confirm your password';
+              }
+              if (value != _passwordController.text) {
+                return 'Passwords do not match';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 32),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // TODO: Submit Form
+                }
+              },
+              child: const Text('Register'),
+            ),
+          ),
+        ],
       ),
     );
   }
