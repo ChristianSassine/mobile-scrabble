c15306f26ab37a4a60134c7d21d26ab307a9c2b0
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2023-03-21 23:49:05 -0400

    Fixed bug

--- lib/domain/services/room-service.dart
+++ lib/domain/services/room-service.dart
@@ -23,6 +23,12 @@ class RoomService {
   Subject<Room> notifyRoomMemberList = PublishSubject();
   Subject<Room> notifyRoomJoin = PublishSubject();
 
+  // TODO: Replace with real image info (Avatar still has to be implemented)
+  final userImage = UserImageInfo(
+      "default-spongebob",
+      true,
+      key: "https://scrabble-images.s3.ca-central-1.amazonaws.com/9e96a2f6-c6f4-4221-bea8-a01f62ba2255default-spongebob.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA5IBYO7WFXIYKAMR6%2F20230322%2Fca-central-1%2Fs3%2Faws4_request&X-Amz-Date=20230322T024825Z&X-Amz-Expires=3600&X-Amz-Signature=516145eb31313f7f4fd1db6d43c3b46bb909d4ec80863930e490f24edada1ebf&X-Amz-SignedHeaders=host&x-id=GetObject");
+
   RoomService() {
     // FOR TESTING
     roomList.add(Room("TEST ROOM", [], "", 0, RoomType.PUBLIC));
@@ -55,20 +61,7 @@ class RoomService {
   }
 
   void requestJoinRoom(Room room) {
-    //TODO SERVER IMPLEMENTATION
     selectedRoom = room;
-
-    // TODO: Replace with real image info (Avatar still has to be implemented)
-    final image = {
-      "name": "default-spongebob",
-      "isDefaultPicture": true,
-      "key":
-          "https://scrabble-images.s3.ca-central-1.amazonaws.com/9e96a2f6-c6f4-4221-bea8-a01f62ba2255default-spongebob.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA5IBYO7WFXIYKAMR6%2F20230322%2Fca-central-1%2Fs3%2Faws4_request&X-Amz-Date=20230322T024825Z&X-Amz-Expires=3600&X-Amz-Signature=516145eb31313f7f4fd1db6d43c3b46bb909d4ec80863930e490f24edada1ebf&X-Amz-SignedHeaders=host&x-id=GetObject"
-    };
-    final userImage = UserImageInfo(
-        image['name'] as String, image['isDefaultPicture'] as bool,
-        key: image['key'] as String);
-    // -------------------------------------------------------------------------
 
     final params = JoinGameParams(
         room.id, IUser(_authService.username!, profilePicture: userImage));
@@ -76,23 +69,18 @@ class RoomService {
   }
 
   void exitWaitingRoom() {
-    debugPrint('Left waiting room');
-
     final payload = {
       "roomId" : selectedRoom!.id,
-      "player" : IUser(_authService.username!)
+      "player" : IUser(_authService.username!, profilePicture: userImage)
     };
     _socket.emit(SocketEvents.ExitWaitingRoom.event, payload);
+
+    debugPrint('Left waiting room');
   }
 
   void _joinRoom(List<IUser> players) {
     selectedRoom!.users = [IUser(_authService.username!), ...players];
-    print("Room Joined");
-  }
-
-  void _receivedRoomList(List<Room> incommingRoomList) {
-    roomList = incommingRoomList;
-    notifyNewRoomList.add(incommingRoomList);
+    debugPrint("Room Joined");
   }
 
   void createRoom(Room room) {
