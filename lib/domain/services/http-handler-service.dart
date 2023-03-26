6b675a096010e571c252e9526ddeebee8026422f
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2023-03-26 00:45:51 -0400

    Adapted desktop + removed library

--- lib/domain/services/http-handler-service.dart
+++ lib/domain/services/http-handler-service.dart
@@ -1,14 +1,11 @@
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 
 class HttpHandlerService {
   late final http.Client client;
   late final String baseUrl;
-  final dio = Dio();
 
   HttpHandlerService(String serverAddress) {
     client = http.Client();
@@ -21,7 +18,8 @@ class HttpHandlerService {
   }
 
   Future<http.Response> signUpRequest(Object body) {
-    return client.post(Uri.parse("${baseUrl}/auth/signUp"), body: body);
+    return client.post(Uri.parse("${baseUrl}/auth/signUp"),
+        headers: {'Content-Type': 'application/json'}, body: body);
   }
 
   // Profile Avatar requests
@@ -31,23 +29,21 @@ class HttpHandlerService {
     );
   }
 
-  Future<Response> sendAvatarRequest(
+  Future<http.StreamedResponse> sendAvatarRequest(
       File image, String key) async {
-    // final request = http.MultipartRequest(
-    //     "POST", Uri.parse("${baseUrl}/image/profile-picture"));
-    //
-    // request.headers['Content-Type'] = 'multipart/form-data';
+    final request = http.MultipartRequest(
+        "POST", Uri.parse("${baseUrl}/image/profile-picture"));
 
-    final imageFile = await MultipartFile.fromFile(image.path, contentType: MediaType("image", "*"));
-    final imageKey = MultipartFile.fromString(key, contentType: MediaType("text", "html"));
+    request.headers['Content-Type'] = 'multipart/form-data';
 
-    // request.files.add(imageKey);
-    // request.files.add(imageFile);
-    // final response = await request.send();
-    final formData = FormData();
+    final imageFile = await http.MultipartFile.fromPath('data', image.path,
+        contentType: MediaType("image", "*"));
 
-    formData.files.addAll([MapEntry('data', imageFile), MapEntry('imageKey', imageKey)]);
-    return dio.post("$baseUrl/image/profile-picture", data: formData);
+    request.files.add(imageFile);
+    request.fields['ImageKey'] = key;
+
+    final response = await request.send();
+    return response;
   }
 
   Future<http.Response> fetchHighScoresRequest() {
