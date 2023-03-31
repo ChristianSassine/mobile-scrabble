f587e2b3921c48d8bd4d1dbf2bc2c10f1b623276
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2023-03-31 04:06:59 -0400

    Implementaded base for statistics and histories in user profile

--- lib/screens/user-profile-screen.dart
+++ lib/screens/user-profile-screen.dart
@@ -1,4 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile/components/indicator-widget.dart';
 import 'package:mobile/screens/user-settings-screen.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -8,7 +10,16 @@ class UserProfileScreen extends StatefulWidget {
   State<UserProfileScreen> createState() => _UserProfileScreenState();
 }
 
-class _UserProfileScreenState extends State<UserProfileScreen> {
+class _UserProfileScreenState extends State<UserProfileScreen>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController =
+      TabController(length: 2, vsync: this);
+
+  final mockData = PieChartData(sections: [
+    PieChartSectionData(title: "${75}", value: 75, color: Colors.red),
+    PieChartSectionData(title: "${25}", value: 25, color: Colors.green)
+  ]);
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -46,19 +57,111 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                       },
                       child: Text("Settings")),
                   Divider(),
-                  Row(
-                    children: [
-                      Container(
-                        width: size.width * 0.2,
-                        height: size.height * 0.5,
-                        color: Colors.green,
-                      ),
-                      const VerticalDivider(),
-                      Container(
-                          width: size.width * 0.2,
-                          height: size.height * 0.5,
-                          color: Colors.red),
-                    ],
+                  IntrinsicHeight(
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Statistics",
+                                style: theme.textTheme.titleLarge,
+                              ),
+                            ),
+                            Text("Temps : 4 m/partie"),
+                            Text("Parties jouées : 100"),
+                            Text("Score moyen par partie : 500"),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Indicator(
+                                      color: Colors.green,
+                                      text: "Win",
+                                      isSquare: false),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Indicator(
+                                      color: Colors.red,
+                                      text: "Loss",
+                                      isSquare: false),
+                                )
+                              ],
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  width: size.width * 0.35,
+                                  child: PieChart(mockData),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const VerticalDivider(),
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Histories",
+                                style: theme.textTheme.titleLarge,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(
+                                      25.0,
+                                    ),
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        child: TabBar(
+                                          indicator: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(
+                                              25.0,
+                                            ),
+                                            color: Colors.green,
+                                          ),
+                                          labelColor: Colors.white,
+                                          unselectedLabelColor: Colors.black,
+                                          controller: _tabController,
+                                          tabs: [
+                                            Tab(
+                                              child: Text("Matches"),
+                                            ),
+                                            Tab(
+                                              child: Text("Connections"),
+                                            )
+                                          ],
+                                        ),
+                                        height: 30,
+                                      ),
+                                      Expanded(
+                                        child: TabBarView(
+                                          controller: _tabController,
+                                          children: [
+                                            Text('Match histories'),
+                                            Text('Connection histories'),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  width: size.width * 0.35,
+                                  height: size.height * 0.55),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   )
                 ],
               ),
