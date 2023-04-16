705c4e9ac367324154812fd4e19d93e41325fb0f
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2023-04-16 03:43:46 -0400

    Mobile fixed modes in creating games

--- lib/components/toggle-icon-button.dart
+++ lib/components/toggle-icon-button.dart
@@ -3,26 +3,40 @@ import 'package:flutter/material.dart';
 class ToggleIconButton extends StatefulWidget {
   final IconData on_icon, off_icon;
   final void Function(bool)? onChanged;
+  final bool isOn;
 
-  const ToggleIconButton({super.key, required this.on_icon, required this.off_icon, this.onChanged});
+  const ToggleIconButton(
+      {super.key,
+      required this.isOn,
+      required this.on_icon,
+      required this.off_icon,
+      this.onChanged});
 
   @override
   _ToggleIconButtonState createState() => _ToggleIconButtonState();
 }
 
 class _ToggleIconButtonState extends State<ToggleIconButton> {
-  bool _isOn = false;
+  late bool _isOn;
+
+  @override
+  void initState() {
+    super.initState();
+    _isOn = widget.isOn;
+  }
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: Icon(_isOn ? widget.off_icon : widget.on_icon),
-      onPressed: () {
-        setState(() {
-          _isOn = !_isOn;
-          if(widget.onChanged != null) widget.onChanged!(_isOn);
-        });
-      },
+      icon: Icon(_isOn ? widget.on_icon : widget.off_icon),
+      onPressed: widget.onChanged == null
+          ? null
+          : () {
+              setState(() {
+                _isOn = !_isOn;
+                if (widget.onChanged != null) widget.onChanged!(_isOn);
+              });
+            },
     );
   }
 }
--- lib/screens/create-game-screen.dart
+++ lib/screens/create-game-screen.dart
@@ -99,152 +99,165 @@ class _GameCreationScreenState extends State<GameCreationScreen> {
       body: Stack(children: [
         ChatButtonWidget(scaffoldKey: _scaffoldKey),
         Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: 300,
-                        child: Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              Text(
-                                  FlutterI18n.translate(
-                                      context, "room_create.room_param_title"),
-                                  style: TextStyle(fontSize: 30)),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  ToggleIconButton(
-                                    off_icon: Icons.lock_outline,
-                                    on_icon: Icons.lock_open_outlined,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        _isProtected = value;
-                                        _formValid = !_isProtected ||
-                                            _roomPasswordController
-                                                .text.isNotEmpty;
-                                      });
-                                    },
-                                  ),
-                                  ToggleIconButton(
-                                    off_icon: Icons.visibility_off,
-                                    on_icon: Icons.visibility,
-                                    onChanged: (value) {
-                                      _isPublic = value;
-                                    },
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                width: 200,
-                                child: Column(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: 300,
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              children: [
+                                Text(
+                                    FlutterI18n.translate(context,
+                                        "room_create.room_param_title"),
+                                    style: TextStyle(fontSize: 30)),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    Visibility(
-                                      visible: _isProtected,
-                                      child: Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 10),
-                                        child: TextFormField(
-                                          controller: _roomPasswordController,
-                                          validator: (value) =>
-                                              value == null || value.isEmpty
-                                                  ? FlutterI18n.translate(
-                                                      context,
-                                                      "form.password_empty")
-                                                  : null,
-                                          decoration: InputDecoration(
-                                            border: OutlineInputBorder(),
-                                            hintText: FlutterI18n.translate(
-                                                context, "form.password"),
-                                            suffixIcon: const Icon(Icons.key),
-                                          ),
-                                          onChanged: (value) {
-                                            setState(() {
-                                              _formValid = value.isNotEmpty;
-                                            });
-                                          },
-                                        ),
-                                      ),
+                                    ToggleIconButton(
+                                      isOn: _isProtected,
+                                      off_icon: Icons.lock_open_outlined,
+                                      on_icon: Icons.lock_outline,
+                                      onChanged: _isPublic
+                                          ? (value) {
+                                              setState(() {
+                                                print("_isProtected : $value");
+                                                _isProtected = value;
+                                                _formValid = !_isProtected ||
+                                                    _roomPasswordController
+                                                        .text.isNotEmpty;
+                                              });
+                                            }
+                                          : null,
                                     ),
-                                    menu.DropdownMenu(
-                                      title:
-                                          "${FlutterI18n.translate(context, "room_create.difficulty_field_title")} *",
-                                      items: {
-                                        FlutterI18n.translate(context,
-                                                "room_create.difficulty.easy"):
-                                            GameDifficulty.Easy.value,
-                                        FlutterI18n.translate(context,
-                                                "room_create.difficulty.hard"):
-                                            GameDifficulty.Hard.value,
-                                        FlutterI18n.translate(context,
-                                                "room_create.difficulty.score_based"):
-                                            GameDifficulty.ScoreBased.value,
-                                      },
-                                      onChanged: (value) {
-                                        _selectedDifficulty =
-                                            GameDifficulty.fromString(value!)!;
-                                      },
-                                      value: GameDifficulty.Easy.value,
-                                    ),
-                                    menu.DropdownMenu(
-                                      title:
-                                          "${FlutterI18n.translate(context, "room_create.timer_field_title")} *",
-                                      items: const {
-                                        "0:30": "30",
-                                        "1:00": "60",
-                                        "1:30": "90",
-                                        "2:00": "120",
-                                        "2:30": "150",
-                                        "3:00": "180",
-                                        "3:30": "210",
-                                        "4:00": "240",
-                                        "4:30": "270",
-                                        "5:00": "300"
-                                      },
-                                      onChanged: (value) {
-                                        _selectedTimer = int.parse(value!);
-                                      },
-                                      value: "60",
-                                    ),
-                                    menu.DropdownMenu(
-                                      title:
-                                          "${FlutterI18n.translate(context, "room_create.dictionary_field_title")} *",
-                                      items: {
-                                        for (var item
-                                            in _dictionaryService.dictionaries)
-                                          item.title: item.title
-                                      },
-                                      onChanged: (value) {
-                                        _selectedDictionary = value;
-                                      },
-                                      value: _selectedDictionary,
+                                    ToggleIconButton(
+                                      isOn: _isPublic,
+                                      off_icon: Icons.visibility_off,
+                                      on_icon: Icons.visibility,
+                                      onChanged: !_isProtected
+                                          ? (value) {
+                                              setState(() {
+                                                print("_isPublic : $value");
+                                                _isPublic = value;
+                                              });
+                                            }
+                                          : null,
                                     ),
                                   ],
                                 ),
-                              ),
-                            ],
+                                SizedBox(
+                                  width: 200,
+                                  child: Column(
+                                    children: [
+                                      Visibility(
+                                        visible: _isProtected,
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(bottom: 10),
+                                          child: TextFormField(
+                                            controller: _roomPasswordController,
+                                            validator: (value) =>
+                                                value == null || value.isEmpty
+                                                    ? FlutterI18n.translate(
+                                                        context,
+                                                        "form.password_empty")
+                                                    : null,
+                                            decoration: InputDecoration(
+                                              border: OutlineInputBorder(),
+                                              hintText: FlutterI18n.translate(
+                                                  context, "form.password"),
+                                              suffixIcon: const Icon(Icons.key),
+                                            ),
+                                            onChanged: (value) {
+                                              setState(() {
+                                                _formValid = value.isNotEmpty;
+                                              });
+                                            },
+                                          ),
+                                        ),
+                                      ),
+                                      menu.DropdownMenu(
+                                        title:
+                                            "${FlutterI18n.translate(context, "room_create.difficulty_field_title")} *",
+                                        items: {
+                                          FlutterI18n.translate(context,
+                                                  "room_create.difficulty.easy"):
+                                              GameDifficulty.Easy.value,
+                                          FlutterI18n.translate(context,
+                                                  "room_create.difficulty.hard"):
+                                              GameDifficulty.Hard.value,
+                                          FlutterI18n.translate(context,
+                                                  "room_create.difficulty.score_based"):
+                                              GameDifficulty.ScoreBased.value,
+                                        },
+                                        onChanged: (value) {
+                                          _selectedDifficulty =
+                                              GameDifficulty.fromString(
+                                                  value!)!;
+                                        },
+                                        value: GameDifficulty.Easy.value,
+                                      ),
+                                      menu.DropdownMenu(
+                                        title:
+                                            "${FlutterI18n.translate(context, "room_create.timer_field_title")} *",
+                                        items: const {
+                                          "0:30": "30",
+                                          "1:00": "60",
+                                          "1:30": "90",
+                                          "2:00": "120",
+                                          "2:30": "150",
+                                          "3:00": "180",
+                                          "3:30": "210",
+                                          "4:00": "240",
+                                          "4:30": "270",
+                                          "5:00": "300"
+                                        },
+                                        onChanged: (value) {
+                                          _selectedTimer = int.parse(value!);
+                                        },
+                                        value: "60",
+                                      ),
+                                      menu.DropdownMenu(
+                                        title:
+                                            "${FlutterI18n.translate(context, "room_create.dictionary_field_title")} *",
+                                        items: {
+                                          for (var item in _dictionaryService
+                                              .dictionaries)
+                                            item.title: item.title
+                                        },
+                                        onChanged: (value) {
+                                          _selectedDictionary = value;
+                                        },
+                                        value: _selectedDictionary,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: _formValid ? _createGame : null,
-                        child: Text(
-                          FlutterI18n.translate(context, "form.create_game"),
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      )
-                    ],
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: _formValid ? _createGame : null,
+                          child: Text(
+                            FlutterI18n.translate(context, "form.create_game"),
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ]),
